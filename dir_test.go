3f0ff8552b23284a09df05c0daa1315ec5a43157
rjbc <rjricky03bc@gmail.com>
2020-07-06 23:15:27 +0800

    add new test and update test

--- dir_test.go
+++ dir_test.go
@@ -1,180 +1,76 @@
 package dir
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCopy(t *testing.T) {
-	type args struct {
-		dest string
-		src  []string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
+func TestNew(t *testing.T) {
+	var tests = []*struct {
+		dir      *Dir
+		expected *Dir
 	}{
-		// TODO: Add test cases.
+		{
+			dir: New("/tmp/stickers/"),
+			expected: &Dir{
+				path:     "/tmp/stickers",
+				nodes:    []string{"tmp", "stickers"},
+				fileName: "",
+			},
+		},
+		{
+			dir: New("/tmp/stickers"),
+			expected: &Dir{
+				path:     "/tmp/stickers",
+				nodes:    []string{"tmp", "stickers"},
+				fileName: "",
+			},
+		},
+		{
+			dir: New("/tmp/stickers/dogs"),
+			expected: &Dir{
+				path:     "/tmp/stickers/dogs",
+				nodes:    []string{"tmp", "stickers", "dogs"},
+				fileName: "",
+			},
+		},
+		{
+			dir: New("/tmp/stickers/sticker.jpg"),
+			expected: &Dir{
+				path:     "/tmp/stickers",
+				nodes:    []string{"tmp", "stickers", "dogs"},
+				fileName: "sticker.jpg",
+			},
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := Copy(tt.args.dest, tt.args.src...); (err != nil) != tt.wantErr {
-				t.Errorf("Copy() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
+	for _, test := range tests {
+		assert.Equal(t, test.expected.path, test.dir.path)
+		assert.Equal(t, test.expected.nodes, test.dir.nodes)
+		assert.Equal(t, test.expected.fileName, test.dir.fileName)
 	}
+}
+
+func TestCopy(t *testing.T) {
 }
 
 func TestCreate(t *testing.T) {
-	type args struct {
-		path      string
-		overwrite bool
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := Create(tt.args.path, tt.args.overwrite); (err != nil) != tt.wantErr {
-				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
 }
 
 func TestDelete(t *testing.T) {
-	type args struct {
-		fullname []string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := Delete(tt.args.fullname...); (err != nil) != tt.wantErr {
-				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
 }
 
 func TestIsExist(t *testing.T) {
-	type args struct {
-		path string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := IsExist(tt.args.path); got != tt.want {
-				t.Errorf("IsExist() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
 
 func TestIsFileExist(t *testing.T) {
-	type args struct {
-		fullname string
-	}
-	tests := []struct {
-		name string
-		args args
-		want bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := IsFileExist(tt.args.fullname); got != tt.want {
-				t.Errorf("IsFileExist() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
 
 func TestList(t *testing.T) {
-	type args struct {
-		path string
-		nest bool
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int
-		want1   []string
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1, err := List(tt.args.path, tt.args.nest)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("List() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("List() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("List() got1 = %v, want %v", got1, tt.want1)
-			}
-		})
-	}
 }
 
 func TestMove(t *testing.T) {
-	type args struct {
-		dest string
-		src  []string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := Move(tt.args.dest, tt.args.src...); (err != nil) != tt.wantErr {
-				t.Errorf("Move() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
 }
 
 func TestPermission(t *testing.T) {
-	type args struct {
-		fullname string
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := Permission(tt.args.fullname); got != tt.want {
-				t.Errorf("Permission() = %v, want %v", got, tt.want)
-			}
-		})
-	}
 }
